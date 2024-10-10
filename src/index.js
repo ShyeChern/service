b374b4e65c677db79b29dad04398aa8fecde277d
ShyeChern <chern-97@hotmail.com>
2024-10-10 18:45:17 +0800

    misc: auth middleware

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const helmet = require('helmet');
 const { rateLimit } = require('express-rate-limit');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
-const middlewares = require('./middlewares');
+const middleware = require('./middlewares');
 const container = require('./container');
 const port = process.env.PORT;
 
@@ -26,14 +26,13 @@ const port = process.env.PORT;
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json());
 	app.use(compression());
-	app.use(middlewares.i18n);
-	app.use(middlewares.createScope);
-	app.use(middlewares.logger);
-	app.use(middlewares.cache);
+	app.use(middleware.i18n);
+	app.use(middleware.createScope);
+	app.use(middleware.logger);
+	app.use(middleware.cache);
 	app.use(cookieParser(process.env.COOKIE_SIGNAGURE));
-	app.use(middlewares.auth);
 	app.use(require('./routes'));
-	app.use(middlewares.error);
+	app.use(middleware.error);
 
 	await container.cradle.init();
 
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const apiV1 = require('./v1/index');
+const middleware = require('./middlewares');
 const { app: appConstant } = require('./constants');
 
-app.use('/api/v1', apiV1);
+app.use('/api/v1', middleware.auth, apiV1);
 
 app.use('/*', (req, res) => {
 	res.status(appConstant.NOT_FOUND).send(`${req.method} ${req.originalUrl} endpoint not found`);
